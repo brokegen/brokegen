ff99ebf3b44b961e04ed09d913ade8762d5b9461
brokegen <5018792+brokegen@users.noreply.github.com>
2024-05-13 21:23:23 +0000

    ollama-proxy: record all request/response data

--- python/access/ratelimits.py
+++ python/access/ratelimits.py
@@ -4,10 +4,18 @@ For potential debugging and caching purposes, offer infrastructure for every net
 Ideally, we would use something like `structlog` or `logging.handlers.HTTPHandler` to send logs elsewhere,
 but this will work for the small scale we have (one user, no automation of LLM requests).
 """
-from collections.abc import Callable
-from typing import TypeAlias
+try:
+    import orjson as json
+except ImportError:
+    import json
 
-from sqlalchemy import create_engine, Column, String, DateTime, JSON, Integer
+import logging
+from collections.abc import Callable, Generator
+from datetime import datetime, timezone
+from typing import TypeAlias, AsyncIterator
+
+import httpx
+from sqlalchemy import create_engine, Column, String, DateTime, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
@@ -24,7 +32,7 @@ def init_db(db_path: str) -> None:
         'sqlite:///' + db_path,
         connect_args={
             "check_same_thread": False,
-            "timeout"          : 1,
+            "timeout": 1,
         })
 
     Base.metadata.create_all(bind=engine)
@@ -33,30 +41,12 @@ def init_db(db_path: str) -> None:
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_db() -> RatelimitsDB:
+def get_db() -> Generator[RatelimitsDB]:
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-
-class ApiAccess(Base):
-    __tablename__ = 'ApiAccesses'
-    __bind_key__ = 'ratelimits'
-
-    accessed_at = Column(DateTime, primary_key=True, nullable=False)
-    api_endpoint = Column(String, primary_key=True, nullable=False)
-
-    api_bucket = Column(String, primary_key=True, nullable=False)
-    response_status_code = Column(Integer)
-
-    def __str__(self):
-        maybe_api_bucket = ""
-        if self.api_bucket:
-            maybe_api_bucket = f" \"{self.api_bucket}\""
-
-        return f"<ApiAccesses{maybe_api_bucket} @ {self.accessed_at}>"
 
 
 class ApiAccessWithResponse(Base):
@@ -69,3 +59,125 @@ class ApiAccessWithResponse(Base):
     api_bucket = Column(String, primary_key=True, nullable=False)
     request = Column(JSON)
     response = Column(JSON)
+
+
+class RequestInterceptor:
+    """
+    Wraps an httpx request/response pair, and stores all that content with SQLAlchemy.
+    """
+
+    def __init__(self, logger: logging.Logger, ratelimits_db: RatelimitsDB):
+        self.logger = logger
+        self.ratelimits_db = ratelimits_db
+        self.new_access: ApiAccessWithResponse | None = None
+
+        self.request_content: bytearray = bytearray()
+
+        self.response_content_bytes: bytearray = bytearray()
+        self.response_content_strs: list[str] = []
+
+    async def wrap_request_content(self, request_content_stream: AsyncIterator[bytes]):
+        async for chunk in request_content_stream:
+            if len(chunk) > 0:
+                self.logger.debug(f"Intercepting request chunk: {len(chunk)=} bytes")
+
+            yield chunk
+            self.request_content.extend(chunk)
+
+    async def consolidate_response_content(self, response: httpx.Response):
+        """
+        Decodes the response into text lines, and then consolidates the streaming JSON into one JSON blob.
+        """
+        raise NotImplementedError()
+
+    async def wrap_response_content(
+            self,
+            response: httpx.Response,
+            disable_json_decode: bool = False,
+    ):
+        async for line in response.aiter_lines():
+            if len(line) > 0:
+                self.logger.debug(f"Intercepting response line: {line[:120]}")
+
+            yield line
+            if disable_json_decode:
+                self.response_content_strs.append(line)
+            else:
+                decoded_line = json.loads(line)
+                self.response_content_strs.append(decoded_line)
+
+    async def wrap_response_content_raw(self, response: httpx.Response):
+        """
+        Don't do any decoding (of gzip, or brotli, or any other HTTP-level compression/encoding
+
+        Mostly useful for direct streaming, with low overhead. Which we don't need yet.
+        """
+        async for chunk in response.aiter_raw():
+            if len(chunk) > 0:
+                self.logger.debug(f"Intercepting encoded response chunk: {len(chunk)=} bytes")
+
+            yield chunk
+            self.response_content_bytes.extend(chunk)
+
+        # Wait for the entire response to be forwarded before erroring out
+        raise NotImplementedError()
+
+    def generate_api_access(
+            self,
+            upstream_response: httpx.Response,
+            api_bucket: str,
+            do_commit: bool = True,
+    ):
+        self.new_access = ApiAccessWithResponse(
+            api_bucket=api_bucket,
+            accessed_at=datetime.now(tz=timezone.utc),
+            api_endpoint=str(upstream_response.request.url),
+            request={
+                'method': upstream_response.request.method,
+                'url': str(upstream_response.request.url),
+                'headers': upstream_response.request.headers.multi_items().sort(),
+                'content': "[not recorded yet]",
+            },
+            response={
+                'status_code': upstream_response.status_code,
+                'headers': upstream_response.headers.multi_items().sort(),
+                'content': "[not recorded yet]",
+            },
+        )
+
+        self.ratelimits_db.add(self.new_access)
+        if do_commit:
+            self.ratelimits_db.commit()
+            # TODO: How important is this to keep around? Will `ratelimits_db.merge()` obviate it?
+            self.ratelimits_db.refresh(self.new_access)
+
+        return self.new_access
+
+    def update_api_access_content(
+            self,
+            do_commit: bool = True,
+    ):
+        """
+        Intended to be called during cleanup, so we can update the DB with content we're done streaming
+        """
+        # https://sqlalche.me/e/20/bhk3
+        merged_access = self.ratelimits_db.merge(self.new_access)
+        # merged_access = self.new_access
+        # self.ratelimits_db.refresh(merged_access)
+
+        if self.request_content:
+            merged_access.request['content'] = self.request_content.decode('utf-8')
+        else:
+            del merged_access.request['content']
+
+        if self.response_content_bytes:
+            # TODO: This assume the response content is UTF-8, we could probably read the encoding from HTTP headers
+            merged_access.response['content'] = self.response_content_bytes.decode('utf-8')
+        elif self.response_content_strs:
+            merged_access.response['content'] = self.response_content_strs
+        else:
+            del merged_access.response['content']
+
+        self.ratelimits_db.add(merged_access)
+        if do_commit:
+            self.ratelimits_db.commit()
--- python/inference/routes.py
+++ python/inference/routes.py
@@ -1,12 +1,11 @@
 import logging
-from datetime import timezone, datetime
 
 import httpx
 from fastapi import APIRouter, Depends, FastAPI, Request
 from starlette.background import BackgroundTask
 from starlette.responses import StreamingResponse
 
-from access.ratelimits import RatelimitsDB, ApiAccessWithResponse
+from access.ratelimits import RatelimitsDB, RequestInterceptor
 from access.ratelimits import get_db as get_ratelimits_db
 
 _real_ollama_client = httpx.AsyncClient(
@@ -22,65 +21,82 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
+async def forward_request_nolog(request: Request):
+    """
+    Implements a simple proxy, as stream-y as possible
+
+    https://github.com/tiangolo/fastapi/issues/1788#issuecomment-1320916419
+    """
+    urlpath_noprefix = request.url.path.removeprefix("/ollama-proxy")
+    logger.debug(f"/ollama-proxy: {request.method} {urlpath_noprefix}")
+
+    url = httpx.URL(path=urlpath_noprefix,
+                    query=request.url.query.encode("utf-8"))
+    rp_req = _real_ollama_client.build_request(
+        request.method,
+        url,
+        headers=request.headers.raw,
+        content=request.stream(),
+    )
+    upstream_response = await _real_ollama_client.send(rp_req, stream=True)
+    return StreamingResponse(
+        upstream_response.aiter_raw(),
+        status_code=upstream_response.status_code,
+        headers=upstream_response.headers,
+        background=BackgroundTask(upstream_response.aclose),
+    )
+
+
+async def forward_request(
+        request: Request,
+        ratelimits_db: RatelimitsDB,
+):
+    intercept = RequestInterceptor(logger, ratelimits_db)
+
+    urlpath_noprefix = request.url.path.removeprefix("/ollama-proxy")
+    logger.debug(f"/ollama-proxy: {request.method} {urlpath_noprefix}")
+
+    url = httpx.URL(path=urlpath_noprefix,
+                    query=request.url.query.encode("utf-8"))
+    upstream_request = _real_ollama_client.build_request(
+        request.method,
+        url,
+        headers=request.headers.raw,
+        content=intercept.wrap_request_content(request.stream()),
+    )
+
+    upstream_response = await _real_ollama_client.send(upstream_request, stream=True)
+    intercept.generate_api_access(upstream_response, api_bucket=__name__)
+
+    async def post_forward_cleanup():
+        await upstream_response.aclose()
+        intercept.update_api_access_content()
+
+    return StreamingResponse(
+        intercept.wrap_response_content(upstream_response),
+        status_code=upstream_response.status_code,
+        headers=upstream_response.headers,
+        background=BackgroundTask(post_forward_cleanup),
+    )
+
+
 def install_proxy_routes(app: FastAPI):
-    async def do_proxy(
-            request: Request,
-            ratelimits_db: RatelimitsDB,
-    ):
-        """
-        Implements a simple reverse proxy, as stream-y as possible
-
-        https://github.com/tiangolo/fastapi/issues/1788#issuecomment-1320916419
-        """
-        urlpath_noprefix = request.url.path.removeprefix("/ollama-proxy")
-        logger.debug(f"/ollama-proxy: {request.method} {urlpath_noprefix}")
-
-        url = httpx.URL(path=urlpath_noprefix,
-                        query=request.url.query.encode("utf-8"))
-        rp_req = _real_ollama_client.build_request(request.method,
-                                                   url,
-                                                   headers=request.headers.raw,
-                                                   content=request.stream())
-        rp_resp = await _real_ollama_client.send(rp_req, stream=True)
-
-        new_access = ApiAccessWithResponse(
-            api_bucket=__name__,
-            accessed_at=datetime.now(tz=timezone.utc),
-            api_endpoint=urlpath_noprefix,
-            request={
-                'method': rp_req.method,
-                'url': str(rp_req.url),
-                'headers': rp_req.headers.multi_items(),
-                # TODO: This is probably safe to read at this point, since we already packaged everything
-                'content': "[skipped + not implemented]",
-            },
-            response={
-                'status_code': rp_resp.status_code,
-                'headers': rp_resp.headers.multi_items(),
-                # Not making a copy of the content, because we should really fork it and not block the StreamingResponse
-                'content': "[skipped + not implemented]",
-            },
-        )
-        ratelimits_db.add(new_access)
-        ratelimits_db.commit()
-
-        return StreamingResponse(
-            rp_resp.aiter_raw(),
-            status_code=rp_resp.status_code,
-            headers=rp_resp.headers,
-            background=BackgroundTask(rp_resp.aclose),
-        )
-
     ollama_forwarder = APIRouter()
+
+    @ollama_forwarder.head("/ollama-proxy/{path:path}")
+    async def do_proxy_head(request: Request):
+        """
+        Don't bother logging HEAD requests, since they aren't _supposed_ to encode anything
+        """
+        return await forward_request_nolog(request)
 
     # TODO: Either OpenAPI or FastAPI doesn't parse these `{path:path}` directives correctly
     @ollama_forwarder.post("/ollama-proxy/{path:path}")
     @ollama_forwarder.get("/ollama-proxy/{path:path}")
-    @ollama_forwarder.head("/ollama-proxy/{path:path}")
-    async def do_proxy_get(
+    async def do_proxy_get_post(
             request: Request,
             ratelimits_db: RatelimitsDB = Depends(get_ratelimits_db),
     ):
-        return await do_proxy(request, ratelimits_db)
+        return await forward_request(request, ratelimits_db)
 
     app.include_router(ollama_forwarder)
