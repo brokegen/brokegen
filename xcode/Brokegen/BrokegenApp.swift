4b29c65c38aefeac0b0c7072b8c602a98d05ba57
brokegen <5018792+brokegen@users.noreply.github.com>
2024-05-14 20:39:17 +0000

    xcode: create new macOS project with Xcode 15.2
    Also remove `UserInterfaceState.xcuserstate` from git.

---
+++ xcode/Brokegen/BrokegenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrokegenApp.swift
+//  Brokegen
+//
+//  Created by user on 2024-05-14.
+//
+
+import SwiftUI
+
+@main
+struct BrokegenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
