7fa8671cf4816731fca7c8e7d583745e862acde4
brokegen <5018792+brokegen@users.noreply.github.com>
2024-05-14 00:29:13 +0000

    rag-proxy: enable output streaming
    Complex part was JSONStreamingResponse, client app was being too Smart

--- python/inference/routes_langchain.py
+++ python/inference/routes_langchain.py
@@ -4,6 +4,7 @@ except ImportError:
     import json
 
 import logging
+from collections.abc import AsyncIterable, Iterable
 
 from faiss import IndexFlatL2
 from fastapi import APIRouter, Depends, FastAPI, Request
@@ -14,13 +15,45 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.llms.ollama import Ollama
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
+from pydantic import BaseModel
+from starlette.background import BackgroundTask
+from starlette.concurrency import iterate_in_threadpool
+from starlette.responses import JSONResponse, StreamingResponse
 
 from access.ratelimits import RatelimitsDB
 from access.ratelimits import get_db as get_ratelimits_db
 from inference.routes import forward_request, forward_request_nodetails
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+
+
+class JSONStreamingResponse(StreamingResponse, JSONResponse):
+    def __init__(
+            self,
+            content: Iterable | AsyncIterable,
+            status_code: int = 200,
+            headers: dict[str, str] | None = None,
+            media_type: str | None = None,
+            background: BackgroundTask | None = None,
+    ) -> None:
+        if isinstance(content, AsyncIterable):
+            self._content_iterable: AsyncIterable = content
+        else:
+            self._content_iterable = iterate_in_threadpool(content)
+
+        async def body_iterator() -> AsyncIterable[bytes]:
+            async for content_ in self._content_iterable:
+                if isinstance(content_, BaseModel):
+                    content_ = content_.model_dump()
+                yield self.render(content_)
+
+        self.body_iterator = body_iterator()
+        self.status_code = status_code
+        if media_type is not None:
+            self.media_type = media_type
+        self.background = background
+        self.init_headers(headers)
 
 
 async def do_transparent_rag(
@@ -35,6 +68,7 @@ Use any sources you can. Some recent context is provided to try and provide newe
 Reasoning: Let's think step by step in order to produce the answer.
 
 Question: {input}""",
+        stream: bool = True,
         embedder_name: str = 'nomic-embed-text:latest',
         embedder_dims: int = 768,
 ):
@@ -42,7 +76,9 @@ Question: {input}""",
     This "transparent" proxy works okay for our purposes (not modifying client code), but is horribly brittle.
     """
     request_content_json = json.loads(await request.body())
-    llm = Ollama(model=request_content_json['model'])
+    llm = Ollama(
+        model=request_content_json['model'],
+    )
 
     prompt = PromptTemplate.from_template(context_template)
     document_chain = create_stuff_documents_chain(llm, prompt)
@@ -57,24 +93,47 @@ Question: {input}""",
     retriever = dummy_vectorstore.as_retriever()
 
     retrieval_chain = create_retrieval_chain(retriever, document_chain)
-    langchain_response = await retrieval_chain.ainvoke({'input': request_content_json['prompt']})
 
-    # TODO: Mirror the Ollama fields as well as we can, without langchain eating the extras
-    ollama_style_response = dict()
-    ollama_style_response['model'] = request_content_json['model']
-    ollama_style_response['response'] = langchain_response['answer']
-    ollama_style_response['done'] = True
+    if stream:
+        async def async_iter():
+            async for chunk in retrieval_chain.astream({
+                'input': request_content_json['prompt'],
+            }):
+                if not chunk.get('answer'):
+                    continue
 
-    if request_content_json.get('context'):
-        ollama_style_response['context'] = request_content_json['context']
+                ollama_style_response = dict()
+                ollama_style_response['model'] = request_content_json['model']
+                # ollama_style_response['created_at'] = datetime.utcnow().isoformat() + 'Z'
+                ollama_style_response['response'] = chunk['answer']
+                ollama_style_response['done'] = False
 
-    # Pretty-print the actual output, since otherwise we'll never know what the True Context was.
-    # TODO: Figure out how to plug in to/modify langchain so we can cache the raw request/response info.
-    logger.info(
-        json.dumps(langchain_response, indent=2)
-    )
+                logger.debug(json.dumps(ollama_style_response))
+                yield ollama_style_response
 
-    return ollama_style_response
+            yield {
+                'model': request_content_json['model'],
+                'response': '',
+                'done': True,
+            }
+
+        return JSONStreamingResponse(
+            async_iter(),
+            status_code=200,
+        )
+
+    else:
+        langchain_response = await retrieval_chain.ainvoke({'input': request_content_json['prompt']})
+
+        ollama_style_response = dict()
+        ollama_style_response['model'] = request_content_json['model']
+        ollama_style_response['response'] = langchain_response['answer']
+        ollama_style_response['done'] = True
+
+        # Pretty-print the actual output, since otherwise we'll never know what the True Context was.
+        # TODO: Figure out how to plug in to/modify langchain so we can cache the raw request/response info.
+        logger.info(json.dumps(langchain_response, indent=2))
+        return ollama_style_response
 
 
 def install_langchain_routes(app: FastAPI):
