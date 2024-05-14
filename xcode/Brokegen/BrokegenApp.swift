aa69ec1d7b4408af488f76763aefd92d8e1c585f
brokegen <5018792+brokegen@users.noreply.github.com>
2024-05-14 21:55:51 +0000

    xcode: update ContentView with output of external process

--- xcode/Brokegen/BrokegenApp.swift
+++ xcode/Brokegen/BrokegenApp.swift
@@ -1,17 +1,26 @@
-//
-//  BrokegenApp.swift
-//  Brokegen
-//
-//  Created by user on 2024-05-14.
-//
-
+import Foundation
 import SwiftUI
 
 @main
 struct BrokegenApp: App {
+    private var proxyProcess: ProxyProcess
+    @State private var proxyOutput: String
+
+    init() {
+        self.proxyProcess = ProxyProcess(["/usr/sbin/ioreg", "-c", "IOPlatformExpertDevice", "-d", "2"])
+        self._proxyOutput = State(initialValue: "[waiting for process to start]")
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                headerText: "ioreg -c IOPlatformExpertDevice",
+                text: $proxyOutput)
+            .task {
+                proxyProcess.launch { result, stdoutData in
+                    self.proxyOutput = String(data: stdoutData, encoding: .utf8)!
+                }
+            }
         }
     }
 }
--- xcode/Brokegen/ContentView.swift
+++ xcode/Brokegen/ContentView.swift
@@ -1,24 +1,50 @@
-//
-//  ContentView.swift
-//  Brokegen
-//
-//  Created by user on 2024-05-14.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var headerText: String
+    @Binding private var text: String
+
+    init(headerText: String, text: Binding<String>) {
+        self.headerText = headerText
+        self._text = text
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text(headerText)
+                .font(.headline)
+            Text(text)
+                .monospaced()
         }
         .padding()
+    }
+
+    public func text(_ text: String) -> ContentView {
+        let view = self
+        view.text = text
+
+        return view
     }
 }
 
 #Preview {
-    ContentView()
+    struct ViewHolder: View {
+        @State private var sampleText = """
+        +-o Root  <class IORegistryEntry, id 0x100000100, retain 38>
+        +-o J316cAP  <class IOPlatformExpertDevice, id 0x100000275, registered, matched, active, busy 0 (659599 ms), retain 41>
+          {
+            "manufacturer" = <"Apple Inc.">
+            "compatible" = <"J316cAP","MacBookPro18,2","AppleARM">
+            "model" = <"MacBookPro18,2">
+          }
+        """
+
+        var body: some View {
+            ContentView(
+                headerText: "/usr/sbin/ioreg -c IOPlatformExpertDevice -d 2",
+                text: $sampleText)
+        }
+    }
+
+    return ViewHolder()
 }
