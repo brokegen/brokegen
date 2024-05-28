76ac86fdb1a15e329a0c2a29e6f10820b1f9c4ac
brokegen <5018792+brokegen@users.noreply.github.com>
2024-05-28 05:11:40 +0000

    /generate: store user messages when Brokegen client submits them

--- python/history/chat/routes_generate.py
+++ python/history/chat/routes_generate.py
@@ -10,7 +10,7 @@ from sqlalchemy import select
 
 import history.ollama
 from _util.json import JSONStreamingResponse
-from _util.typing import ChatSequenceID, PromptText, InferenceModelHumanID
+from _util.typing import ChatSequenceID, PromptText, InferenceModelHumanID, ChatMessageID
 from audit.http import AuditDB
 from audit.http import get_db as get_audit_db
 from history.chat.database import ChatMessageOrm, ChatSequence, lookup_chat_message, ChatMessageAddRequest
@@ -37,25 +37,48 @@ def construct_router():
             history_db: HistoryDB = Depends(get_history_db),
             audit_db: AuditDB = Depends(get_audit_db),
     ) -> JSONStreamingResponse:
+        original_sequence = history_db.execute(
+            select(ChatSequence)
+            .filter_by(id=params.sequence_id)
+        ).scalar_one()
+
         # Manually fetch the message + model config history from our requests
         messages_list: list[ChatMessageOrm] = \
             do_get_sequence(params.sequence_id, history_db, include_model_info_diffs=False)
 
         # First, store the message that was painstakingly generated for us.
+        user_sequence = ChatSequence(
+            human_desc=original_sequence.human_desc,
+            parent_sequence=original_sequence.id,
+            generated_at=datetime.now(tz=timezone.utc),
+            generation_complete=False,
+        )
+
         if params.user_prompt and params.user_prompt.strip():
             new_message = ChatMessageOrm(
                 role='user',
                 content=params.user_prompt,
-                created_at=datetime.now(tz=timezone.utc),
+                created_at=user_sequence.generated_at,
             )
             maybe_message = lookup_chat_message(ChatMessageAddRequest.from_orm(new_message), history_db)
             if maybe_message is not None:
                 messages_list.append(maybe_message)
-
+                user_sequence.current_message = maybe_message.id
+                user_sequence.generation_complete = True
             else:
                 history_db.add(new_message)
                 history_db.commit()
                 messages_list.append(new_message)
+                user_sequence.current_message = new_message.id
+                user_sequence.generation_complete = True
+
+            # Mark this user response as the current up-to-date
+            user_sequence.user_pinned = original_sequence.user_pinned
+            original_sequence.user_pinned = False
+
+            history_db.add(original_sequence)
+            history_db.add(user_sequence)
+            history_db.commit()
 
         # Fetch the InferenceModelHumanID from latest ChatSequence
         inference_id: InferenceEventID = history_db.execute(
@@ -82,6 +105,12 @@ def construct_router():
         constructed_request._body = orjson.dumps(constructed_ollama_body)
 
         # Wrap the output in a… something that appends new ChatSequence information
+        partial_response_sequence = ChatSequence(
+            human_desc=original_sequence.human_desc,
+            parent_sequence=user_sequence.id or original_sequence.id,
+            generation_complete=False
+        )
+
         async def add_json_suffix(primordial):
             done_signaled: int = 0
             async for chunk in primordial:
