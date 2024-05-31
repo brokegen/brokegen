328a8a75b7f35858bae2af8361b6686ba7a0578d
brokegen <5018792+brokegen@users.noreply.github.com>
2024-05-31 14:31:20 +0000

    xcode wip: further tweaks to SV2

--- xcode/Brokegen/History/SequenceViewTwo.swift
+++ xcode/Brokegen/History/SequenceViewTwo.swift
@@ -2,8 +2,10 @@ import Combine
 import CustomTabView
 import SwiftUI
 
+let darkBackgroundStyle = Color(.black)
+
 enum Tab: String, Hashable, CaseIterable {
-    case hidden, simple, configurable, advanced
+    case simple, pro, proMax, options
 }
 
 struct ComposeTabsView: View {
@@ -17,19 +19,44 @@ struct ComposeTabsView: View {
 
     var body: some View {
         VStack(alignment: .leading) {
-            Spacer()
+            ForEach([Tab.simple, Tab.pro, Tab.proMax], id: \.self) { tab in
+                HStack {
+                    Spacer()
+                        .frame(minWidth: 0)
+                    Text(tab.rawValue)
+                        .layoutPriority(1)
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .frame(maxHeight: 32)
+                .background(selection == tab
+                            ? Color.accentColor
+                            : darkBackgroundStyle)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    selection = tab
+                    onTabSelection(tab)
+                }
+            }
 
-            ForEach(Tab.allCases, id: \.self) { tab in
-                Label(tab.rawValue, systemImage: selection == tab ? "circle.fill" : "circle")
+            VStack {
+                Spacer()
+
+                Text("options")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .frame(maxHeight: 32)
+                    .background(selection == .options
+                                ? Color.accentColor
+                                : Color(.controlBackgroundColor))
                     .contentShape(Rectangle())
                     .onTapGesture {
-                        selection = tab
-                        onTabSelection(tab)
+                        selection = .options
+                        onTabSelection(.options)
                     }
-
-                Spacer()
             }
         }
+        .frame(maxWidth: 80)
     }
 }
 
@@ -43,114 +70,114 @@ struct SequenceViewTwo: View {
     }
 
     var body: some View {
-        List {
-            ForEach(viewModel.sequence.messages) { message in
-                OneMessageView(message)
-                    .padding(24)
-                    .padding(.top, 16)
+        ScrollViewReader { proxy in
+            ScrollView(.vertical) {
+                ForEach(viewModel.sequence.messages) { message in
+                    OneMessageView(message)
+                        .padding(24)
+                        .padding(.top, 16)
+                }
+
+                if viewModel.responseInEdit != nil {
+                    OneMessageView(viewModel.responseInEdit!)
+                        .padding(24)
+                        .padding(.top, 16)
+                }
             }
-
-            if viewModel.responseInEdit != nil {
-                OneMessageView(viewModel.responseInEdit!)
-                    .padding(24)
-                    .padding(.top, 16)
-            }
-        }
-
-        let composeTabsView = ComposeTabsView(selection: $selectedTab) { tab in
-            print("Picked tab \(tab.rawValue)")
-        }
-            .frame(maxHeight: .infinity)
-
-        CustomTabView(tabBarView: composeTabsView, tabs: Tab.allCases, selection: selectedTab) {
-            Rectangle()
-                .fill(Color.clear)
+            .layoutPriority(0.5)
 
             HStack {
-                InlineTextInput($viewModel.promptInEdit, isFocused: $focusTextInput)
-                    .padding(.top, 24)
-                    .padding(.bottom, 24)
-                    .border(.blue)
-                    .disabled(viewModel.submitting || viewModel.responseInEdit != nil)
-                    .onSubmit {
-                        viewModel.requestExtend()
-                    }
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                            self.focusTextInput = true
-                        }
-                    }
+                Text("Ollama status: loading model")
+                Spacer()
+            }
+            .foregroundStyle(Color(.disabledControlTextColor))
 
-                Group {
-                    //                Button(action: viewModel.stopSubmitAndReceive) {
-                    //                    let icon: String = {
-                    //                        if viewModel.submitting || viewModel.responseInEdit != nil {
-                    //                            return "stop.fill"
-                    //                        }
-                    //                        else {
-                    //                            return "stop"
-                    //                        }
-                    //                    }()
-                    //                    Image(systemName: icon)
-                    //                        .resizable()
-                    //                        .frame(width: 32, height: 32)
-                    //                        .disabled(!viewModel.submitting && viewModel.responseInEdit == nil)
-                    //                }
-                    //                .buttonStyle(.plain)
-                    //                .help("Stop submitting or receiving")
+            let composeTabsView = ComposeTabsView(selection: $selectedTab) { tab in
+                print("Picked tab \(tab.rawValue)")
+            }
+                .frame(maxHeight: .infinity)
 
-                    Button(action: viewModel.requestExtendWithRetrieval) {
-                        Image(systemName: viewModel.submitting ? "arrowshape.up.fill" : "arrowshape.up")
-                            .resizable()
-                            .frame(width: 32, height: 32)
+            CustomTabView(tabBarView: composeTabsView, tabs: Tab.allCases, selection: selectedTab) {
+                VStack {
+                    HStack {
+                        InlineTextInput($viewModel.promptInEdit, isFocused: $focusTextInput)
+                            .padding(.top, 24)
+                            .padding(.bottom, 24)
+                            .border(.blue)
                             .disabled(viewModel.submitting || viewModel.responseInEdit != nil)
+                            .onSubmit {
+                                viewModel.requestExtend()
+                            }
+                            .onAppear {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                    self.focusTextInput = true
+                                }
+                            }
+
+                        Group {
+                            Button(action: viewModel.requestExtendWithRetrieval) {
+                                Image(systemName: viewModel.submitting ? "arrowshape.up.fill" : "arrowshape.up")
+                                    .resizable()
+                                    .frame(width: 32, height: 32)
+                                    .disabled(viewModel.submitting || viewModel.responseInEdit != nil)
+                            }
+                            .buttonStyle(.plain)
+                            .help("Submit with Retrieval")
+                        }
+                        .padding(.top, 16)
+                        .padding(.bottom, 12)
+                        .padding(.leading, 12)
+                        .padding(.trailing, 12)
                     }
-                    .buttonStyle(.plain)
-                    .help("Submit with Retrieval-Augmented Generation")
-
-                    //                Button(action: viewModel.requestExtend) {
-                    //                    Image(systemName: viewModel.submitting ? "arrow.up.circle.fill" : "arrow.up.circle")
-                    //                        .resizable()
-                    //                        .frame(width: 32, height: 32)
-                    //                        .disabled(viewModel.submitting || viewModel.responseInEdit != nil)
-                    //                }
-                    //                .buttonStyle(.plain)
-                    //                .help("Submit")
+                    Spacer()
                 }
-                .padding(.top, 16)
-                .padding(.bottom, 12)
-                .padding(.leading, 12)
-                .padding(.trailing, -12)
-            }
-            .tabItem {
-                Text("normal")
-            }
 
-            HStack {
-                Text("content")
-            }
-            .tabItem {
                 HStack {
-                    Label("  Tab 2", systemImage: "2.circle")
+                    Text("content")
                 }
-                .padding(24)
-            }
+                .tabItem {
+                    HStack {
+                        Label("  Tab 2", systemImage: "2.circle")
+                    }
+                    .padding(24)
+                }
 
-            VStack {
-                Text("more content")
+                VStack {
+                    Text("more content")
+                }
+                .tabItem {
+                    Label("Privacy", systemImage: "hand.raised")
+                }
+
+                VStack {
+                    VStack(alignment: .leading) {
+                        Text("llama-3-120B")
+                            .font(.title)
+                            .monospaced()
+                            .foregroundColor(.accentColor)
+                            .lineLimit(2)
+                            .padding(.bottom, 8)
+
+                        Divider()
+                        Text("response sec/token: 0.04")
+                            .lineLimit(1...)
+                            .monospaced()
+                            .padding(4)
+                    }
+                    .padding(12)
+                    .listRowSeparator(.hidden)
+                    .padding(.bottom, 48)
+                    .frame(maxWidth: 800)
+                }
             }
-            .tabItem {
-                Label("Privacy", systemImage: "hand.raised")
-            }
+            .tabBarPosition(.edge(.leading))
+            .layoutPriority(0.2)
         }
-        .tabBarPosition(selectedTab == .hidden ? .floating(.leading) : .edge(.leading))
-//        .frame(width: 800, height: 200)
-        .padding(.leading, 24)
-        .padding(.trailing, 24)
+        .background(darkBackgroundStyle)
     }
 }
 
-#Preview(traits: .fixedLayout(width: 800, height: 400)) {
+#Preview(traits: .fixedLayout(width: 800, height: 1200)) {
     let messages: [Message] = [
         Message(role: "user", content: "First message", createdAt: Date.distantPast),
         Message(role: "clown", content: "Second message", createdAt: Date.distantPast),
