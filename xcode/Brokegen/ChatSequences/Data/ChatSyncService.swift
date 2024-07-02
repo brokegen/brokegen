cfed68d6c7096b724d5dd83ab1014dfde38cc2d3
brokegen <5018792+brokegen@users.noreply.github.com>
2024-07-02 17:40:37 +0000

    xcode: update ChatSyncService.updateSequenceOffline

--- xcode/Brokegen/ChatSequences/Data/ChatSyncService.swift
+++ xcode/Brokegen/ChatSequences/Data/ChatSyncService.swift
@@ -196,6 +196,8 @@ class ChatSyncService: ObservableObject {
         for clientModel in matchingClientModels {
             clientModel.sequence = updatedSequence
         }
+
+        objectWillChange.send()
     }
 
     // MARK: - ChatSequence extend/continue
