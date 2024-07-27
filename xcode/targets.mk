7747c6586fc2d9f26766096eaf251d48fdb16fa5
brokegen <5018792+brokegen@users.noreply.github.com>
2024-07-27 17:11:58 +0000

    xcode: build distinct debug/release targets

--- xcode/targets.mk
+++ xcode/targets.mk
@@ -7,14 +7,18 @@ build/xcode-macos-export-options.plist:
 	plutil -insert "method" -string "mac-application" "$@"
 
 .PHONY: dist-xcode
-dist: dist-xcode
 dist-xcode: build-xcode
 dist-xcode: build/xcode-macos-export-options.plist
 	xcodebuild -quiet \
-			-exportArchive \
-			-archivePath build/"macOS App.xcarchive" \
-			-exportPath dist/ \
-			-exportOptionsPlist build/xcode-macos-export-options.plist
+		-exportArchive \
+		-archivePath build/"macOS App.xcarchive" \
+		-exportPath dist/ \
+		-exportOptionsPlist build/xcode-macos-export-options.plist
+	# TODO: This clean-up should be less manual. Or within the xcode build.
+	rm dist/Brokegen.app/Contents/Resources/brokegen-server
+	rm dist/Brokegen.app/Contents/Resources/ollama-darwin
+	rm -rf dist/"Brokegen (Debug)".app
+	mv dist/Brokegen.app dist/"Brokegen (Debug)".app
 
 .PHONY: build-xcode
 build: build-xcode
@@ -23,16 +27,43 @@ build-xcode:
 	xcodebuild archive \
 		-quiet \
 		-project xcode/Brokegen.xcodeproj \
+		-scheme Debug \
+		-config Debug \
+		-sdk macosx \
+		-archivePath build/"macOS App.xcarchive" \
+		-derivedDataPath build/xcode-derived-data/
+
+.PHONY: dist-xcode-release
+dist: dist-xcode-release
+dist-xcode-release: build-xcode-release
+dist-xcode-release: build/xcode-macos-export-options.plist
+	xcodebuild -quiet \
+		-exportArchive \
+		-archivePath build/"macOS App (Release).xcarchive" \
+		-exportPath dist/ \
+		-exportOptionsPlist build/xcode-macos-export-options.plist
+	# TODO: This clean-up should be less manual. Or within the xcode build.
+	rm -rf dist/"Brokegen (Release)".app
+	mv dist/Brokegen.app dist/"Brokegen (Release)".app
+
+.PHONY: build-xcode-release
+build: build-xcode-release
+build-xcode-release: server
+build-xcode-release:
+	xcodebuild archive \
+		-quiet \
+		-project xcode/Brokegen.xcodeproj \
 		-scheme Release \
 		-config Release \
 		-sdk macosx \
-		-archivePath build/"macOS App.xcarchive" \
+		-archivePath build/"macOS App (Release).xcarchive" \
 		-derivedDataPath build/xcode-derived-data/
 
 .PHONY: clean-xcode
 clean: clean-xcode
 clean-xcode:
 	rm -rf build/xcode-derived-data/
+	rm -rf build/"macOS App.xcarchive"/
 	rm -rf build/"macOS App.xcarchive"/
 	rm -f build/xcode-macos-export-options.plist
 	rm -rf dist/Brokegen.app
