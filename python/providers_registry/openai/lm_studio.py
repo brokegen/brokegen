84d5e6d3fea25a54d009be8e1336899846bd0353
brokegen <5018792+brokegen@users.noreply.github.com>
2025-02-04 18:40:24 +0000

    lm_studio wip: actually add record responses
    Known not working:
    
    - RAG isn't checked/implemented, not likely to be working
    - saw some tokens that seem out-of-order, but that seems to be on the
      model/LM Studio side

--- python/providers_registry/openai/lm_studio.py
+++ python/providers_registry/openai/lm_studio.py
@@ -1,34 +1,76 @@
 import logging
 import os.path
 from datetime import datetime, timezone
-from typing import AsyncGenerator, AsyncIterator, Awaitable, TypeVar, Callable
+from typing import AsyncGenerator, AsyncIterator, Awaitable, TypeVar
 
 import fastapi
 import httpx
 import orjson
-from _util.json import JSONDict, safe_get
+import sqlalchemy
+from _util.json import JSONDict, safe_get, safe_get_arrayed
 from _util.status import ServerStatusHolder
 from _util.typing import ChatSequenceID, PromptText
 from audit.http import AuditDB
 from audit.http import get_db as get_audit_db
 from audit.http_raw import HttpxLogger
 from client.database import HistoryDB, get_db as get_history_db
-from client.message import ChatMessage
+from client.message import ChatMessage, ChatMessageOrm
+from client.sequence import ChatSequenceOrm
 from client.sequence_get import fetch_messages_for_sequence
-from inference.iterators import stream_str_to_json, consolidate_and_call, consolidate_and_yield
+from inference.iterators import stream_str_to_json, consolidate_and_yield, tee_to_console_output
+from inference.logging import construct_assistant_message
 from providers.foundation_models.orm import FoundationModelRecord, InferenceEventOrm
 from providers.foundation_models.orm import lookup_foundation_model_detailed, \
     FoundationModelAddRequest, FoundationModelRecordOrm
 from providers.orm import ProviderRecordOrm, ProviderLabel, ProviderRecord, ProviderType
 from providers.registry import ProviderRegistry, BaseProvider, ProviderFactory, InferenceOptions
 from providers_registry._util import local_provider_identifiers, local_fetch_machine_info
-from providers_registry.ollama.api_chat.logging import ollama_response_consolidator
 from sqlalchemy import select
 
 logger = logging.getLogger(__name__)
 
 T = TypeVar('T')
 U = TypeVar('U')
+
+
+def chat_completion_choice0_extractor(chunk: JSONDict) -> str:
+    response_choices = safe_get(chunk, "choices")
+    if len(response_choices) > 1:
+        logger.warning(f"Received {len(response_choices)=}, ignoring all but the first")
+
+    return safe_get_arrayed(response_choices, 0, 'delta', 'content') or ""
+
+
+def openai_response_consolidator(
+        chunk: JSONDict,
+        consolidated_response: JSONDict,
+) -> JSONDict:
+    """
+    https://platform.openai.com/docs/api-reference/chat/streaming
+
+    TODO: Check the values of each individual field
+    """
+    if not consolidated_response:
+        return chunk
+
+    for k, v in chunk.items():
+        if k not in consolidated_response:
+            consolidated_response[k] = v
+            continue
+
+        elif k == 'choices':
+            if len(v) > 1:
+                logger.warning(f"Received {len(v)} completion choices, ignoring all but the first")
+
+            if not safe_get_arrayed(consolidated_response, k, 0, 'delta', 'content'):
+                consolidated_response[k][0]['delta']['content'] = safe_get_arrayed(v, k, 0, 'delta', 'content') or ""
+            else:
+                consolidated_response[k][0]['delta']['content'] += safe_get_arrayed(v, k, 0, 'delta', 'content') or ""
+
+        # In the non-exceptional case, just update with the new value.
+        consolidated_response[k] = v
+
+    return consolidated_response
 
 
 class LMStudioProvider(BaseProvider):
@@ -203,6 +245,127 @@ class LMStudioProvider(BaseProvider):
             history_db: HistoryDB,
             audit_db: AuditDB,
     ) -> AsyncIterator[JSONDict]:
+        async def jsonner(
+                primordial0: AsyncIterator[bytes],
+        ) -> AsyncIterator[JSONDict]:
+            async for chunk in primordial0:
+                # The first few bytes of the lm_studio response always start with b'data: '
+                if chunk[0:6] == b'data: ':
+                    chunk = chunk[6:]
+
+                yield orjson.loads(chunk)
+
+        async def update_status(
+                primordial: AsyncIterator[JSONDict],
+        ) -> AsyncIterator[JSONDict]:
+            chunk_number: int = 0
+
+            async for chunk in primordial:
+                chunk_number += 1
+                status_holder.set(f"[lm_studio] {inference_model.human_id}: received response chunk #{chunk_number}")
+
+                if "status" not in chunk:
+                    chunk["status"] = status_holder.get()
+
+                yield chunk
+
+            status_holder.set(f"[lm_studio] {inference_model.human_id}: done with inference")
+
+        async def reformat_openai(
+                primordial: AsyncIterator[JSONDict],
+        ) -> AsyncIterator[JSONDict]:
+            async for chunk in primordial:
+                current_time = datetime.now(tz=timezone.utc)
+                yield {
+                    "model": inference_model.human_id,
+                    "created_at": current_time.isoformat() + "Z",
+                    "done": False,
+                    "message": {
+                        "role": "assistant",
+                        "content": chat_completion_choice0_extractor(chunk),
+                    }
+                }
+
+        async def append_response_chunk(
+                consolidated_response: JSONDict,
+        ) -> AsyncIterator[JSONDict]:
+            # And now, construct the ChatSequence (which references the InferenceEvent, actually)
+            try:
+                inference_event = InferenceEventOrm(
+                    model_record_id=inference_model.id,
+                    prompt_with_templating=None,
+                    response_created_at=datetime.now(tz=timezone.utc),
+                    response_info=consolidated_response,
+                    reason="LMStudioProvider.do_chat_logged",
+                )
+
+                history_db.add(inference_event)
+                history_db.commit()
+
+                response_message: ChatMessageOrm | None = construct_assistant_message(
+                    maybe_response_seed=inference_options.seed_assistant_response or "",
+                    assistant_response=safe_get(consolidated_response, "message", "content") or "",
+                    created_at=inference_event.response_created_at,
+                    history_db=history_db,
+                )
+                if not response_message:
+                    return
+
+                original_sequence: ChatSequenceOrm = history_db.execute(
+                    select(ChatSequenceOrm)
+                    .where(ChatSequenceOrm.id == sequence_id)
+                ).scalar_one()
+
+                # TODO: Replace with `construct_new_sequence_from`
+                response_sequence = ChatSequenceOrm(
+                    human_desc=original_sequence.human_desc,
+                    user_pinned=False,
+                    current_message=response_message.id,
+                    parent_sequence=original_sequence.id,
+                )
+
+                history_db.add(response_sequence)
+
+                response_sequence.generated_at = inference_event.response_created_at
+                response_sequence.generation_complete = True
+                response_sequence.inference_job_id = inference_event.id
+                if inference_event.response_error:
+                    response_sequence.inference_error = inference_event.response_error
+
+                history_db.commit()
+
+                # And complete the circular reference that really should be handled in the SQLAlchemy ORM
+                inference_job = history_db.merge(inference_event)
+                inference_job.parent_sequence = response_sequence.id
+
+                history_db.add(inference_job)
+                history_db.commit()
+
+                # Return fields that the client probably cares about
+                yield {
+                    "new_message_id": response_sequence.current_message,
+                    "new_sequence_id": response_sequence.id,
+                    "done": True,
+                }
+
+            except sqlalchemy.exc.SQLAlchemyError:
+                logger.exception(f"Failed to create add-on ChatSequence from {consolidated_response}")
+                history_db.rollback()
+                status_holder.set(f"Failed to create add-on ChatSequence from {consolidated_response}")
+                yield {
+                    "error": "Failed to create add-on ChatSequence",
+                    "done": True,
+                }
+
+            except Exception:
+                logger.exception(f"Failed to create add-on ChatSequence from {consolidated_response}")
+                status_holder.set(f"Failed to create add-on ChatSequence from {consolidated_response}")
+                yield {
+                    "error": "Failed to create add-on ChatSequence",
+                    "done": True,
+                }
+
+        # TODO: make system messages/overrides available here
         messages_list: list[ChatMessage] = fetch_messages_for_sequence(sequence_id, history_db)
 
         request_content = {
@@ -213,26 +376,6 @@ class LMStudioProvider(BaseProvider):
             message.model_dump() for message in messages_list
         ]
         request_content['model'] = inference_model.human_id
-
-        inference_event = InferenceEventOrm(
-            model_record_id=inference_model.id,
-            prompt_with_templating=None,
-            response_created_at=datetime.now(tz=timezone.utc),
-            response_error="[haven't received/finalized response info yet]",
-            reason=None,
-        )
-        history_db.add(inference_event)
-        history_db.commit()
-
-        def do_finalize_inference_job(response_content):
-            merged_inference_event = history_db.merge(inference_event)
-            if safe_get(response_content, 'error'):
-                inference_event.response_error = safe_get(response_content, 'error', 'message')
-            else:
-                inference_event.response_error = None
-
-            history_db.add(merged_inference_event)
-            history_db.commit()
 
         with HttpxLogger(self.server_comms, next(get_audit_db())):
             headers = httpx.Headers()
@@ -250,14 +393,17 @@ class LMStudioProvider(BaseProvider):
             upstream_response: httpx.Response
             upstream_response = await self.server_comms.send(request, stream=True)
 
-            iter0: AsyncIterator[str] = upstream_response.aiter_text()
-            iter1: AsyncIterator[JSONDict] = stream_str_to_json(iter0)
-            iter2: AsyncIterator[JSONDict] = consolidate_and_yield(
-                iter1, ollama_response_consolidator, {},
-                do_finalize_inference_job,
+            iter0: AsyncIterator[bytes] = upstream_response.aiter_bytes()
+            iter1: AsyncIterator[JSONDict] = jsonner(iter0)
+            iter2: AsyncIterator[JSONDict] = tee_to_console_output(iter1, chat_completion_choice0_extractor)
+            iter3: AsyncIterator[JSONDict] = consolidate_and_yield(
+                iter2, openai_response_consolidator, {},
+                append_response_chunk,
             )
+            iter4: AsyncIterator[JSONDict] = reformat_openai(iter3)
+            iter5: AsyncIterator[JSONDict] = update_status(iter4)
 
-            return iter2
+            return iter5
 
 
 class LMStudioFactory(ProviderFactory):
