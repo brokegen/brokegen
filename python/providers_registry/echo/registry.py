001200f2d69db969788a11c1a76bcac618de5b2d
brokegen <5018792+brokegen@users.noreply.github.com>
2025-02-11 23:48:31 +0000

    echo: fix implementation to use ChatMessage/ChatSequences
    Prior to this, nothing was ever committed to history_db, so the UI never
    quite recorded/showed output correctly.

--- python/providers_registry/echo/registry.py
+++ python/providers_registry/echo/registry.py
@@ -3,18 +3,22 @@ import functools
 from datetime import datetime, timezone
 from typing import AsyncGenerator, AsyncIterator
 
+import sqlalchemy
 from _util.json import JSONDict, safe_get
 from _util.status import ServerStatusHolder
-from _util.typing import PromptText, TemplatedPromptText
+from _util.typing import PromptText, TemplatedPromptText, ChatSequenceID
 from audit.http import AuditDB
-from client.message import ChatMessage
+from client.message import ChatMessage, ChatMessageOrm
 from client.database import HistoryDB, get_db as get_history_db
-from inference.iterators import tee_to_console_output, consolidate_and_call
+from client.sequence import ChatSequenceOrm
+from client.sequence_get import fetch_messages_for_sequence
+from inference.iterators import tee_to_console_output, consolidate_and_call, consolidate_and_yield
 from inference.logging import construct_new_sequence_from
 from providers.foundation_models.orm import FoundationModelRecord, FoundationModelAddRequest, \
     lookup_foundation_model_detailed, FoundationModelRecordOrm
 from providers.orm import ProviderType, ProviderLabel, ProviderRecord, ProviderID
 from providers.registry import BaseProvider, ProviderRegistry, ProviderFactory, InferenceOptions
+from sqlalchemy import select
 
 
 async def _chat_bare(
@@ -49,20 +53,13 @@ async def _chat_slowed_down(
             "done": False,
         }
 
-    await asyncio.sleep(1.0)
-    yield {
-        "status": status_holder.get(),
-        "done": True,
-        # "new_sequence_id": -5,
-    }
-
 
 def echo_consolidator(chunk: JSONDict, consolidated_response: JSONDict) -> JSONDict:
     if not consolidated_response:
         return chunk
 
     message: str = safe_get(chunk, "message", "content")
-    consolidated_response["message"]["content"] = message
+    consolidated_response["message"]["content"] += message or ""
 
     return consolidated_response
 
@@ -110,8 +107,9 @@ class EchoProvider(BaseProvider):
 
             yield FoundationModelRecord.model_validate(new_model)
 
-    async def do_chat_nolog(
+    async def do_chat(
             self,
+            sequence_id: ChatSequenceID,
             messages_list: list[ChatMessage],
             inference_model: FoundationModelRecordOrm,
             inference_options: InferenceOptions,
@@ -119,14 +117,76 @@ class EchoProvider(BaseProvider):
             history_db: HistoryDB,
             audit_db: AuditDB,
     ) -> AsyncIterator[JSONDict]:
+        original_sequence = history_db.execute(
+            select(ChatSequenceOrm)
+            .where(ChatSequenceOrm.id == sequence_id)
+        ).scalar_one()
+
+        async def record_new_sequence(
+                consolidated_response: JSONDict,
+        ) -> AsyncIterator[JSONDict]:
+            yield {
+                "status": "Committing new response from \"echo\" provider",
+                "done": False,
+            }
+
+            try:
+                new_echo_message = ChatMessageOrm(
+                    role="assistant",
+                    content=safe_get(consolidated_response, "message", "content"),
+                    created_at=datetime.now(tz=timezone.utc),
+                )
+                history_db.add(new_echo_message)
+                history_db.flush()
+
+                # Add what we need for response_sequence
+                response_sequence = ChatSequenceOrm(
+                    human_desc=original_sequence.human_desc,
+                    user_pinned=False,
+                    current_message=new_echo_message.id,
+                    parent_sequence=original_sequence.id,
+                )
+
+                history_db.add(response_sequence)
+
+                response_sequence.generated_at = new_echo_message.created_at
+                response_sequence.generation_complete = True
+                response_sequence.inference_job_id = None
+
+                history_db.commit()
+
+                await asyncio.sleep(1.0)
+                yield {
+                    "status": status_holder.get(),
+                    "done": True,
+                    "new_message_id": new_echo_message.id,
+                    "new_sequence_id": response_sequence.id,
+                }
+
+            except sqlalchemy.exc.SQLAlchemyError:
+                history_db.rollback()
+                status_holder.set(f"Failed to create add-on ChatSequence from {consolidated_response}")
+                yield {
+                    "error": "Failed to create add-on ChatSequence",
+                    "done": True,
+                }
+
+            except Exception:
+                status_holder.set(f"Failed to create add-on ChatSequence from {consolidated_response}")
+                yield {
+                    "error": "Failed to create add-on ChatSequence",
+                    "done": True,
+                }
+
+        messages_list: list[ChatMessage] = fetch_messages_for_sequence(sequence_id, history_db)
         message_text: PromptText = messages_list[-1].content
 
         iter0: AsyncIterator[str] = _chat_bare(message_text)
         iter1: AsyncIterator[str] = tee_to_console_output(iter0, lambda s: s)
         iter2: AsyncIterator[JSONDict] = _chat_slowed_down(iter1, status_holder)
-        iter3: AsyncIterator[JSONDict] = consolidate_and_call(
+        iter3: AsyncIterator[JSONDict] = consolidate_and_yield(
             iter2, echo_consolidator, {},
-            functools.partial(construct_new_sequence_from, history_db=history_db),
+            record_new_sequence,
         )
 
         return iter3
@@ -143,12 +203,8 @@ class EchoProvider(BaseProvider):
         iter0: AsyncIterator[str] = _chat_bare(prompt)
         iter1: AsyncIterator[str] = tee_to_console_output(iter0, lambda s: s)
         iter2: AsyncIterator[JSONDict] = _chat_slowed_down(iter1, status_holder)
-        iter3: AsyncIterator[JSONDict] = consolidate_and_call(
-            iter2, echo_consolidator, {},
-            functools.partial(construct_new_sequence_from, history_db=history_db),
-        )
 
-        return iter3
+        return iter2
 
 
 class EchoProviderFactory(ProviderFactory):
